13673e9baeb6673cb8be6cfbbaca0233d13ecae1
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-27 14:59:58 -0300

    Created sections for hard skills

--- src/components/main/HardSkills.jsx
+++ src/components/main/HardSkills.jsx
@@ -1,78 +1,46 @@
+import { useState } from "react";
+import Extras from "./skills/Extras";
+import Frontend from "./skills/Frontend";
+import Backend from "./skills/Backend";
+
 const HardSkills = () => {
+  const [skills, setSkills] = useState("Frontend");
   return (
-    <>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg"
-          alt="JS Logo"
-        />
-        <h4 className="js-color mt-1">JavaScript</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
-          alt="HTML Logo"
-        />
-        <h4 className="html-color mt-1">HTML</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
-          alt="CSS Logo"
-        />
-        <h4 className="css-color mt-1">CSS</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg"
-          alt="React Logo"
-        />
-        <h4 className="react-color mt-1">React.js</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/materialui-colored.svg"
-          alt="MUI Logo"
-        />
-        <h4 className="mui-color mt-1">Material UI</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg"
-          alt="Bootstrap Logo"
-        />
-        <h4 className="bootstrap-color mt-1">Bootstrap</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/csharp-colored.svg"
-          alt="C# Logo"
-        />
-        <h4 className="c-sharp-color mt-1">C#</h4>
-      </div>
-      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
-        <img
-          style={{ width: "90px", height: "90px" }}
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-          alt="Google Logo"
-        />
-        <h4 className="c-sharp-color mt-1">
-          <span className="g-color">G</span>
-          <span className="oe-color">o</span>
-          <span className="o2-color">o</span>
-          <span className="g-color">g</span>
-          <span className="l-color">l</span>
-          <span className="oe-color">e</span>
+    <div className="row d-flex justify-content-center">
+      <div className="d-flex justify-content-evenly mb-4">
+        <h4
+          className="skills-btn"
+          style={{
+            color: skills == "Frontend" ? "#B115E0" : "#2CB67D",
+          }}
+          onClick={() => setSkills("Frontend")}
+        >
+          Frontend
+        </h4>
+        <h4
+          className="skills-btn"
+          style={{
+            color: skills == "Backend" ? "#B115E0" : "#2CB67D",
+          }}
+          onClick={() => setSkills("Backend")}
+        >
+          Backend
+        </h4>
+        <h4
+          className="skills-btn"
+          style={{
+            color: skills == "Extras" ? "#B115E0" : "#2CB67D",
+          }}
+          onClick={() => setSkills("Extras")}
+        >
+          Extras
         </h4>
       </div>
-    </>
+
+      {skills == "Frontend" && <Frontend />}
+      {skills == "Backend" && <Backend />}
+      {skills == "Extras" && <Extras />}
+    </div>
   );
 };
 
---
+++ src/components/main/skills/Extras.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+const Extras = () => {
+  return (
+    <div className="row d-flex justify-content-center enter">
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/csharp-colored.svg"
+          alt="C# Logo"
+        />
+        <h4 className="c-sharp-color mt-1">C#</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+          alt="Jest Logo"
+        />
+        <h4 className="jest-color mt-1">Jest</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg"
+          alt="Mocha Logo"
+        />
+        <h4 className="mocha-color mt-1">Mocha</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://vitest.dev/logo.svg"
+          alt="Vitest Logo"
+        />
+        <h4 className="vite-color mt-1">Vitest</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://testing-library.com/img/octopus-128x128.png"
+          alt="RTL Logo"
+        />
+        <h4 className="rtl-color mt-1 text-center">React Testing Library</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg"
+          alt="Redux Logo"
+        />
+        <h4 className="redux-color mt-1 text-center">Redux</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://img.icons8.com/color/344/firebase.png"
+          alt="Firebase Logo"
+        />
+        <h4 className="firebase-color mt-1 text-center">Firebase</h4>
+      </div>
+      <div className="d-flex flex-column align-items-center col-6 col-sm-6 col-md-3 col-lg-3 p-2">
+        <img
+          style={{ width: "90px", height: "90px" }}
+          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+          alt="Google Logo"
+        />
+        <h4 className="c-sharp-color mt-1">
+          <span className="g-color">G</span>
+          <span className="oe-color">o</span>
+          <span className="o2-color">o</span>
+          <span className="g-color">g</span>
+          <span className="l-color">l</span>
+          <span className="oe-color">e</span>
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Extras;
